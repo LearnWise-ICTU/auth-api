ef7327fd640f94eb9d71bf9c29a77307be0de979
Fon Godwill Ivan Tana <ivan8tana@gmail.com>
2024-07-25 10:51:17 +0100

    feat: Add email functionality for user registration and verification

--- services/email.py
+++ services/email.py
@@ -1,19 +1,39 @@
 from models.user import User
 import mailtrap as mt
 import smtplib
+import jinja2
 
 from config.config import Settings
+
+
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+
+
+env = Environment(
+    loader=FileSystemLoader("./templates"),
+    autoescape=select_autoescape(["html", "xml"]),
+)
 
 
 async def send_account_verification_email(user: User):
     sender = "ivan8tana@gmail.com"
     receiver = user.email
 
+    template = env.get_template("user/account-verification.html")
+
+    html_content = template.render(
+        name=user.full_name,
+        activate_url="https://example.com/activate",
+        app_name="LearnWise",
+    )
+
     mail = mt.Mail(
-        sender=mt.Address(email="mailtrap@demomailtrap.com", name="Mailtrap Test"),
+        sender=mt.Address(
+            email="mailtrap@demomailtrap.com", name="Account  Verification"
+        ),
         to=[mt.Address(email="ivan8tana@gmail.com")],
-        subject="You are awesome!",
-        text="Congrats for sending test email with Mailtrap!",
+        subject="Account Varification!",
+        html=html_content,
         category="Integration Test",
     )
 
